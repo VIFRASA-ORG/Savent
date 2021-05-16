f34fc47a766dd4783ad3d393abfe774311a43292
zcloc <zclock612@gmail.com>
2021-05-16 13:03:53 +0200

    Collegamento schermata login-registrazione con la schermata login. Traduzione della doppia lingua nella schermata di login.

--- app/src/main/java/com/vitandreasorino/savent/LogSingInActivity.java
+++ app/src/main/java/com/vitandreasorino/savent/LogSingInActivity.java
@@ -2,7 +2,9 @@ package com.vitandreasorino.savent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LogSingInActivity extends AppCompatActivity {
 
@@ -12,5 +14,15 @@ public class LogSingInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logsignin);
     }
+
+
+    public void onLoginClick(View view) {
+        
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+
 
 }
