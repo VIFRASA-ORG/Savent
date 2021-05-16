a3fa052d1f651d522f95038b99cdc18912d5423c
zcloc <zclock612@gmail.com>
2021-05-16 12:24:50 +0200

    Creazione della schermata login.

---
+++ app/src/main/java/com/vitandreasorino/savent/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.vitandreasorino.savent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+
+
+
+}
