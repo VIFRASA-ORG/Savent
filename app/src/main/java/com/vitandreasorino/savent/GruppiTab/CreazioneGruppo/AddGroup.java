c17731681754435f7fec4913465503b501404b89
Sara Scarcelli <sara.scarcelli_1998@libero.it>
2021-06-17 13:02:01 +0200

    Miglioramento delle schermate di Creazione gruppo e Aggiunta contatti con implementazione della selezione dei contatti mediante checkbox per la creazione del gruppo.

--- app/src/main/java/com/vitandreasorino/savent/GruppiTab/CreazioneGruppo/AddGroup.java
+++ app/src/main/java/com/vitandreasorino/savent/GruppiTab/CreazioneGruppo/AddGroup.java
@@ -1,26 +1,40 @@
 package com.vitandreasorino.savent.GruppiTab.CreazioneGruppo;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.vitandreasorino.savent.R;
 
+import java.util.ArrayList;
+
+import Model.ContactModel;
+
 public class AddGroup extends AppCompatActivity {
+
     FloatingActionButton buttonNewContactGroup;
+    ImageView imageNewGroup;
+    RecyclerView recyclerView;
+    ContactAdapter adapter;
+    ArrayList<ContactModel> contactsGroupList = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_group);
+
+        recyclerView = findViewById(R.id.contactGroupList);
+
     }
-
-
-
     /**
-     * metodo che richiama la successiva classe abbinata allo scopo di aggiungere un nuovo membro al gruppo
+     * metodo che richiama la successiva classe abbinata allo scopo di aggiungere nuovi contatti al gruppo
      * cliccando il floating button "aggiungi contatto"
      * @param view
      */
@@ -38,4 +52,34 @@ public class AddGroup extends AppCompatActivity {
         finish();
     }
 
+    public void onClickPhoto(View view){
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+
+        // pass the constant to compare it
+        // with the returned requestCode
+        startActivityForResult(Intent.createChooser(i, "Select Picture"), 200);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+
+            // compare the resultCode with the
+            // SELECT_PICTURE constant
+            if (requestCode == 200) {
+                // Get the url of the image from data
+                Uri selectedImageUri = data.getData();
+                if (null != selectedImageUri) {
+                    // update the preview image in the layout
+                    // Compress it before it is shown into the imageView
+                    imageNewGroup.setImageURI(selectedImageUri);
+                }
+            }
+        }
+    }
 }
